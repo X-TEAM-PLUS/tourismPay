50730f89e7426abe570d5529c8683ad48a96946c
songxinlei <songxinlei@paypalm.cn>
2017-10-18 14:07:40 +0800

    银联通道开发

---
+++ tourismpay-unionpay/src/main/java/com/xteam/tourismpay/api/service/UnionPayThirdPartyPaymentServiceImpl.java
@@ -0,0 +1,13 @@
+package com.xteam.tourismpay.api.service;
+
+import java.util.Map;
+
+/**
+ * Created by xinleisong on 2017/10/13.
+ */
+public class UnionPayThirdPartyPaymentServiceImpl implements ThirdPartyPaymentService {
+    @Override
+    public Map<String, Object> getQRCode(String out_trade_no, String total_amount, String subject, String productId) {
+        return null;
+    }
+}
---
+++ tourismpay-unionpay/src/test/java/com/xteam/tourismpay/api/service/impl/TestUnionPayThirdPartyPaymentServiceImpl.java
@@ -0,0 +1,7 @@
+package com.xteam.tourismpay.api.service.impl;
+
+/**
+ * Created by xinleisong on 2017/10/13.
+ */
+public class TestUnionPayThirdPartyPaymentServiceImpl {
+}
